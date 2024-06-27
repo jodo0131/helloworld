3a6da9131bec899e7ea2216b2de12edbf0ea10f1
jodo0131 <168631774+jodo0131@users.noreply.github.com>
2024-06-27 18:11:20 +0800

    Update HelloController.java

--- src/main/java/com/jrrtugs/hello_world_demo/HelloController.java
+++ src/main/java/com/jrrtugs/hello_world_demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.jrrtugs.hello_world_demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +9,13 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
+        model.addAttribute("message", "Hello World! This is a sample application");
         return "index"; // Assuming "index.html" is your homepage template
+    }
+
+    @GetMapping("/")
+    public String handleError() {
+        return "error"; // for error page
     }
 
 }
